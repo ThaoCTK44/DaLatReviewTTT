1c504bb924548825cb11d0865c0fe91eb009e60d
DangPhuongTay <89719935+DangPhuongTay@users.noreply.github.com>
2024-01-31 16:32:24 +0700

    login

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,9 @@
 const headerBody = document.querySelector('body'); 
+const btnLogin1 = document.querySelector('.login2');
+const btnLogin2 = document.querySelector('.login2');
+const btnLogout = document.querySelector('.header__top--right__item.logout');
 const mobileMenuBtn = document.querySelector('.home__banner--mobile__bottom');
+const loginChecker = document.querySelector('.header__top--right__item--checker');
 const hamburgerBtn = document.querySelector('.header__top--left__hamburger');
 const btnCloseLoginSignForm = document.querySelector('.login__signup--box__close');
 const btnOpenLoginSignFormPc1 = document.querySelector('.header__top--right__item.open__login--signup__form');
@@ -22,6 +26,17 @@ function mobileMenu() {
 hamburgerBtn.addEventListener('click',()=>{
     headerBody.classList.toggle('active');
 })
+
+btnLogin2.addEventListener('click',()=>{
+    headerBody.classList.add('checker');
+})
+btnLogin1.addEventListener('click',()=>{
+    headerBody.classList.add('checker');
+})
+btnLogout.addEventListener('click',()=>{
+    headerBody.classList.remove('checker');
+})
+
 btnOpenLoginSignForm.addEventListener('click',()=>{
     headerBody.classList.add('LoginSignUpForm');
 })
