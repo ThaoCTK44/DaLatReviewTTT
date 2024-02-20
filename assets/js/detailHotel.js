816b057fc6cb4bb742b01c637071d4b4c7dd6e38
TienDat <95626306+LMTTienDat@users.noreply.github.com>
2024-02-20 15:43:31 +0700

    update detailHotel

--- assets/js/detailHotel.js
+++ assets/js/detailHotel.js
@@ -1,5 +1,8 @@
 const btnDetailHotelRate = document.querySelector('.detailHotel__info--summary__left.rate');
 const btnDetailHotelRate2 = document.querySelector('.detailHotel__list--body__reviews--content__overview--link.rate');
+const btnDetailHotelPrice = document.querySelector('.detailHotel__list--body__list--wrap__item--right__body.price');
+const btnDetailHotelPrice2 = document.querySelector('.detailHotel__list--body__list--wrap__item--right__body.prices');
+const closeDetailHotelPrice = document.querySelector('.detailHotel__list--body__list--wrapper__icon ion-icon');
 const closeDetailHotelRate = document.querySelector('.detailHotel__list--body__reviews--wrap__icon ion-icon');
 const closeDetailHotelBlur = document.querySelector('.detailHotel__backgroup--blur');
 
@@ -12,6 +15,19 @@ btnDetailHotelRate2.addEventListener('click', ()=>{
     detailHotelBody.classList.add('listDetailHotelRate2')
     detailHotelBody.classList.add('detailHotelBlur')
 })
+btnDetailHotelPrice.addEventListener('click', ()=>{
+    detailHotelBody.classList.add('listDetailHotelPrice')
+    detailHotelBody.classList.add('detailHotelBlur')
+})
+btnDetailHotelPrice2.addEventListener('click', ()=>{
+    detailHotelBody.classList.add('listDetailHotelPrice2')
+    detailHotelBody.classList.add('detailHotelBlur')
+})
+closeDetailHotelPrice.addEventListener('click', ()=>{
+  detailHotelBody.classList.remove('listDetailHotelPrice')
+  detailHotelBody.classList.remove('listDetailHotelPrice2')
+
+})
 closeDetailHotelRate.addEventListener('click', ()=>{
     detailHotelBody.classList.remove('listDetailHotelRate')
     detailHotelBody.classList.remove('listDetailHotelRate2')
@@ -19,8 +35,9 @@ closeDetailHotelRate.addEventListener('click', ()=>{
 closeDetailHotelBlur.addEventListener('click', ()=>{
     detailHotelBody.classList.remove('listDetailHotelRate')
     detailHotelBody.classList.remove('listDetailHotelRate2')
+    detailHotelBody.classList.remove('listDetailHotelPrice')
+    detailHotelBody.classList.remove('listDetailHotelPrice2')
     detailHotelBody.classList.remove('detailHotelBlur')
-
 })
 
 
