5506143b6df7e16bf353569913beb0090d4a4ef8
DangPhuongTay <89719935+DangPhuongTay@users.noreply.github.com>
2024-02-29 13:13:37 +0700

    update detail car/ update link

--- assets/js/detail_car.js
+++ assets/js/detail_car.js
@@ -1,4 +1,4 @@
-
+const openMapCarFormBtn = document.querySelector('.detail__car--info__map');
 const closeInfoCarFormBlur = document.querySelector('.header__background--blur__fullview');
 const closeInfoCarFormBtn = document.querySelector('.detail__car--info__header--icon');
 const detailCarBody = document.querySelector('body');
@@ -7,9 +7,16 @@ const detailCarBody = document.querySelector('body');
 function openInfoCarFormBtn(){
     detailCarBody.classList.add('InfoCarForm');
 }
+openMapCarFormBtn.addEventListener('click',()=>{
+    detailCarBody.classList.add('MapCarForm');
+})
 closeInfoCarFormBtn.addEventListener('click',()=>{
     detailCarBody.classList.remove('InfoCarForm');
 })
+function closeMapCarFormBtn(){
+    detailCarBody.classList.remove('MapCarForm');
+}
+
 closeInfoCarFormBlur.addEventListener('click',()=>{
     detailCarBody.classList.remove('InfoCarForm');
 })
